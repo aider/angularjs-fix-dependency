6b0a6a58f88903bc313384f15f9a2990b8ffb83d
GitHub Action <action@github.com>
2021-10-19 20:38:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/aider/angularjsfixdependency/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/aider/angularjsfixdependency/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.aider.angularjsfixdependency.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.aider.angularjsfixdependency.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/aider/angularjsfixdependency/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.aider.angularjsfixdependency.services
+
+import com.github.aider.angularjsfixdependency.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
